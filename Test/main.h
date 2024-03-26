0cef0b314dc7deaedbd07f6ce77181d3c4585ed8
Goliath33 <gauliath@msn.com>
2024-03-26 11:50:07 +0100

    add protypes and function on main.h and create syntax-printf.c

--- Test/main.h
+++ Test/main.h
@@ -1,6 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+
+//PROTOTYPE _printf
+void _printf(const char *format, ...);
+
+//PROTOTYPE _putchar
+int _putchar(char c);
+
+//PROTOTYPE print_character
+int print_character(va_list ap);
+int print_string(va_list ap);
+int print_pourcentage(va_list ap);
+int print_integer(va_list ap);
 
 #endif
