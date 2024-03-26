f7473f84ce0ef952e756feb7b671a4f769973316
root <root@Kvin2601>
2024-03-26 11:50:01 +0100

    add function _putchar

--- Test/main.h
+++ Test/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
+int print_character(va_list ap);
+int print_string(va_list ap);
+int print_pourcentage(va_list ap);
+int print_integer(va_list ap);
 
 
 #endif
