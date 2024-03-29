91a659393cbc8dc1de37bb346844915c3187ac9d
root <root@Kvin2601>
2024-03-29 13:18:32 +0100

    deletion of spaces in place of tabs

--- syntaxe-printf.c
+++ syntaxe-printf.c
@@ -1,27 +1,24 @@
 #include "main.h"
 
 /**
-* get_function - choose the corresponding function
-* @s: check to the string
-* @list: check to the list
-* Return: corresponding function or NULL
-*/
-
+ * get_function - choose the corresponding function
+ * @s: check to the string
+ * Return: corresponding function or NULL
+ */
 int (*get_function(char s))(va_list)
 {
 	int i = 0;
 
-	choice_c list[] =
-     {
-{'c', print_character},
-{'s', print_string},
-{'%', print_pourcentage},
-{'\0', NULL},
-     };
+	choice_c list[] = {
+		{'c', print_character},
+		{'s', print_string},
+		{'%', print_pourcentage},
+		{'\0', NULL},
+	};
 
 	while (list[i].f != NULL)
 	{
-	 if (s == list[i].letter)
+		if (s == list[i].letter)
 		{
 			return (list[i].f);
 		}
